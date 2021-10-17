9f54b4452bd05076f11d5bfd59be5c5c4ce91866
Saurabh_Katkade <skatkade7@gmail.com>
2021-10-17 13:06:07 +0530

    First Commit - implemented empty string,single digit sum,two digit sum and newline deilimeter

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,28 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	String [] num = input.split(",|\n");
+    	if(input.isEmpty())
+    	{
+    		 return 0;
+    	}
+    	
+    	if(input.length()==1)
+    	{
+    		return Integer.parseInt(input);
+    	}
+    	else
+    	{
+    		return getSum(num[0] , num[1]);
+    	}
+    	
+    	
     }
+
+	private int getSum(String numone,String numtwo) {
+		return Integer.parseInt(numone) + Integer.parseInt(numtwo);
+	}
+
+	
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -7,14 +7,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalculatorShould {
 
     @Test
-    void empty_string_should_return_0() {
+    void empty_string_should_return_0() {                             // Empty String Returns Zero
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    void string_with_single_number_should_return_number_as_int() {     //  Single String Ineger Returns Integer Itself
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+    void string_with_two_number_should_return_number_as_sum_of_two() {     //   Returns Integer Sum of Two
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1,2"));
+    }
+    
+    @Test
+    void two_numbers_newline_delimetted_sum() {     //  Newline delimiter Returns Integer Sum of Two
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1\n2"));
+    }
+    
+   
+    
+    
 }
